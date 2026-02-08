119e432db087674aa682c5279831b8cbb7b79265
ROllerozxa <rollerozxa@voxelmanip.se>
2026-02-08 16:19:50 +0100

    Build SDL from source, try to get ARM64 builds working

--- build.sh
+++ build.sh
@@ -2,9 +2,46 @@
 
 # Builds an AppImage using appimagetool.
 # You need to run this inside of a build directory in the source tree,
-# it will generate the build files and compile Minetest for you.
+# it will generate the build files and compile Luanti for you.
 
 # This script should be run on Debian 11 Bullseye.
+
+get_tar_archive() {
+	# $1: folder to extract to, $2: URL
+	local filename="${2##*/}"
+	[ -d "$1" ] && return 0
+
+	wget -nc -c "$2" -O "$filename"
+	mkdir -p "$1"
+	tar -xaf "$filename" -C "$1" --strip-components=1
+}
+
+# Ensure the architecture argument is set correctly
+arch="${1:-x86_64}"
+: "${LOCAL_RUN:=0}"
+
+# COMPILE SDL2
+sdl_ver="2.32.10"
+get_tar_archive SDL2 "https://github.com/libsdl-org/SDL/releases/download/release-${sdl_ver}/SDL2-${sdl_ver}.tar.gz"
+
+pushd SDL2
+
+mkdir -p build; cd build
+cmake .. -G Ninja \
+	-DSDL_INSTALL_CMAKEDIR=usr/lib/cmake/SDL2 \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX=/ \
+	-DCMAKE_C_FLAGS="-DSDL_LEAN_AND_MEAN=1" \
+	-DSDL_{AUDIO,RENDER,VULKAN,TEST,STATIC}=OFF
+ninja
+strip -s *.so
+if [ "${LOCAL_RUN}" != "1" ]; then
+	DESTDIR="/" ninja install
+else
+	DESTDIR="../../" ninja install
+fi
+
+popd
 
 # COMPILE LUAJIT
 pushd luajit
@@ -16,9 +53,7 @@ mkdir -p build; cd build
 
 # Download appimagetool
 if [ ! -f appimagetool ]; then
-	# Old version of appimagetool:
-	#wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
-	wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
+	wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-${arch}.AppImage -O appimagetool
 	chmod +x appimagetool
 fi
 
@@ -30,14 +65,12 @@ cmake .. -G Ninja \
 	-DENABLE_SYSTEM_JSONCPP=OFF \
 	-DLUA_INCLUDE_DIR=../../luajit/src/ \
 	-DLUA_LIBRARY=../../luajit/src/libluajit.a
-ninja
-
-objcopy --only-keep-debug ../bin/luanti luanti.debug
-objcopy --strip-debug --add-gnu-debuglink=luanti.debug ../bin/luanti
-
-ninja install
+ninja; ninja install
 
 cd AppDir
+
+objcopy --only-keep-debug usr/bin/luanti ../luanti.debug
+objcopy --strip-debug --add-gnu-debuglink=../luanti.debug usr/bin/luanti
 
 # Put desktop and icon at root
 ln -sf usr/share/applications/org.luanti.luanti.desktop luanti.desktop
@@ -73,13 +106,20 @@ INCLUDE_LIBS=(
 )
 
 mkdir -p usr/lib/
-for i in "${INCLUDE_LIBS[@]}"; do
-	cp /usr/lib/x86_64-linux-gnu/$i usr/lib/
-done
+
+if [ "${LOCAL_RUN}" != "1" ]; then
+	for i in "${INCLUDE_LIBS[@]}"; do
+		cp /usr/lib/${arch}-linux-gnu/$i usr/lib/
+	done
+fi
 
 # Copy our own built SDL2
-cp /usr/lib/libSDL2-2.0.so.0 usr/lib/
+if [ "${LOCAL_RUN}" != "1" ]; then
+	cp /usr/lib/libSDL2-2.0.so.0 usr/lib/
+else
+	cp ../../../usr/lib/libSDL2-2.0.so.0 usr/lib/
+fi
 
 # Actually build the appimage
 cd ..
-ARCH=x86_64 ./appimagetool --appimage-extract-and-run AppDir/
+ARCH=${arch} ./appimagetool --appimage-extract-and-run AppDir/
